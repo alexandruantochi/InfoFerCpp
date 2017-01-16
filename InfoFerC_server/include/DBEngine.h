d82c8c84f7ece343af2a4ac5968fb035a934e880
alexandru.antochi <alexandru.antochi@gmail.com>
2017-01-16 21:58:20 +0200

    fileHandler created

--- InfoFerC_server/include/DBEngine.h
+++ InfoFerC_server/include/DBEngine.h
@@ -1,14 +1,31 @@
 #ifndef DBENGINE_H_INCLUDED
 #define DBENGINE_H_INCLUDED
 #include <iostream>
+#include <string>
 #include <sqlite3.h>
 #include <map>
 #include <vector>
 
+
+
+
 void DBEngineInit(const char *location);
+//connect to the database
 
 void executeQuery(std::string query, int client_id);
+//execute the given query from the SQLQueue
+
 
 int queryResolver(void *client_id, int argc, char **argv, char **azColName);
+//get the results, parse them
+
+
+void resetResults();
+//reset results, used everytime a new query is executed
+
+
+int fileGenerator(int client_id, std::map<std::string, std::string>);
+//generate the file to send it to the client
+
 
 #endif // DBENGINE_H_INCLUDED
---
+++ InfoFerC_server/include/fileHandler.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+#include <vector>
+
+
+int fileGenerator(int client_id, std::map<std::string, std::string> resultMap);
